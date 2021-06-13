29e18515b5986b07509fa65e9fb718a814801f54
wiktorowski.michal <222609@edu.p.lodz.pl>
2021-06-13 22:28:49 +0200

    test 6 green

--- src/main/java/SimplifiedTennisGame.java
+++ src/main/java/SimplifiedTennisGame.java
@@ -1,11 +1,21 @@
+import java.util.Arrays;
+
 public class SimplifiedTennisGame {
 
     public static final String INITIAL_VERBAL_SCORE = "Love all";
     public static final String DEUCE = "Deuce";
+    private final String firstPlayer;
+    private final String secondPlayer;
     private String verbalScore;
 
     public SimplifiedTennisGame() {
+        this("no_name", "no_name2");
         verbalScore = INITIAL_VERBAL_SCORE;
+    }
+
+    public SimplifiedTennisGame(String firstPlayer, String secondPlayer) {
+        this.firstPlayer = firstPlayer;
+        this.secondPlayer = secondPlayer;
     }
 
     public String getVerbalScore() {
@@ -22,6 +32,8 @@ public class SimplifiedTennisGame {
                 return "Thirty";
             case 3:
                 return "Forty";
+            case 4:
+                return "Game";
         }
         return null;
     }
@@ -33,6 +45,10 @@ public class SimplifiedTennisGame {
             verbalScore = INITIAL_VERBAL_SCORE;
         else if (firstPlayerScore == secondPlayerScore) {
             verbalScore = findVerbalWhenScoresAreEqual(firstPlayerScore);
+        } else if (firstPlayerScore == 4) {
+            verbalScore = "Game " + Arrays.stream(firstPlayer.split(" ")).toArray()[1];
+        } else if (secondPlayerScore == 4) {
+            verbalScore = "Game " + Arrays.stream(secondPlayer.split(" ")).toArray()[1];
         } else
             verbalScore = findMatch(firstPlayerScore) + ", " + findMatch(secondPlayerScore);
     }
