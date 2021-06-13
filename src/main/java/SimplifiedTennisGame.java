1e1525d0592506a56e675632b0bda5d935f53784
wiktorowski.michal <222609@edu.p.lodz.pl>
2021-06-13 19:58:40 +0200

    test 1 green

--- src/main/java/SimplifiedTennisGame.java
+++ src/main/java/SimplifiedTennisGame.java
@@ -1,2 +1,14 @@
 public class SimplifiedTennisGame {
+
+    private String firstPlayer;
+    private String secondPlayer;
+
+    public SimplifiedTennisGame(String firstPlayer, String secondPlayer) {
+        this.firstPlayer = firstPlayer;
+        this.secondPlayer = secondPlayer;
+    }
+
+    public String getCurrentScore() {
+        return "Love all";
+    }
 }
