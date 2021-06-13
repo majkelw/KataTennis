0f1d5e255970927adc1c2b4d4771e2d992a3a1f9
wiktorowski.michal <222609@edu.p.lodz.pl>
2021-06-13 19:56:52 +0200

    test 1 red

--- src/test/java/SimplifiedTennisGameTest.java
+++ src/test/java/SimplifiedTennisGameTest.java
@@ -1,9 +1,12 @@
 import org.junit.Test;
-
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 public class SimplifiedTennisGameTest {
 
-    @Test
-    public void itCompiles(){
 
+    @Test
+    public void scoreShouldBeLoveAll_atTheBeginningOfTheGame() {
+        SimplifiedTennisGame game = new SimplifiedTennisGame("John Smith", "Andy Davis");
+        assertThat(game.getCurrentScore(), equalTo("Love all"));
     }
 }
